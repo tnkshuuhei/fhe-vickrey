d63b0d8a10c2806c0a2640309571f16e69ddb154
shuhei tanaka <shuhei46491@gmail.com>
2024-04-19 10:14:07 +0900

    import dotenv

--- tasks/deployERC20.ts
+++ tasks/deployERC20.ts
@@ -1,10 +1,25 @@
+import dotenv from "dotenv";
 import { task } from "hardhat/config";
 import type { TaskArguments } from "hardhat/types";
 
+dotenv.config();
+
+const privateKey = process.env.PRIVATE_KEY as string;
+
 task("task:deployERC20").setAction(async function (taskArguments: TaskArguments, { ethers }) {
-  const signers = await ethers.getSigners();
+  // const signers = await ethers.getSigners();
+  if (!privateKey) {
+    throw new Error("Please set your PRIVATE_KEY in a .env file");
+  }
+  const provider = new ethers.JsonRpcProvider("https://devnet.zama.ai");
+  const wallet = new ethers.Wallet(privateKey, provider);
+  const signer = wallet.connect(provider);
   const erc20Factory = await ethers.getContractFactory("EncryptedERC20");
-  const encryptedERC20 = await erc20Factory.connect(signers[0]).deploy("Naraggara", "NARA"); // City of Zama's battle);
+  const encryptedERC20 = await erc20Factory.connect(signer).deploy("Naraggara", "NARA"); // City of Zama's battle);
   await encryptedERC20.waitForDeployment();
   console.log("EncryptedERC20 deployed to: ", await encryptedERC20.getAddress());
+
+  const tx = await encryptedERC20.connect(signer).mint(10000);
+  await tx.wait();
+  console.log("Minted 10000 NARA to: ", await signer.getAddress(), "hash: ", tx.hash);
 });
--- tasks/deployVickreyAuction.ts
+++ tasks/deployVickreyAuction.ts
@@ -4,15 +4,13 @@ import type { TaskArguments } from "hardhat/types";
 task("task:deployVickreyAuction").setAction(async function (taskArguments: TaskArguments, { ethers }) {
   const signers = await ethers.getSigners();
   const vickreyAuctionFactory = await ethers.getContractFactory("VickreyAuction");
-  const vickreyAuction = await vickreyAuctionFactory
-    .connect(signers[0])
-    .deploy(
-      "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
-      "0xC45bc9c0E228da1Ef9cc6F0Ee4631F7e1998da69",
-      "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
-      1713608515,
-      true,
-    ); // City of Zama's battle);
+  const vickreyAuction = await vickreyAuctionFactory.connect(signers[0]).deploy(
+    "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
+    "0xC45bc9c0E228da1Ef9cc6F0Ee4631F7e1998da69",
+    "0x06aa005386F53Ba7b980c61e0D067CaBc7602a62",
+    604800, // 1week
+    true,
+  ); // City of Zama's battle);
   await vickreyAuction.waitForDeployment();
   console.log("VickreyAuction deployed to: ", await vickreyAuction.getAddress());
 });
