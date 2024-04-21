3a95534264fa8706eb3c61ac9d3c293f52c2db4a
shuhei tanaka <shuhei46491@gmail.com>
2024-04-21 16:24:52 +0900

    fix auctionFactory contract

--- contracts/AuctionFactory.sol
+++ contracts/AuctionFactory.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 
 import "./BlindAuction.sol";
 import "./VickreyAuction.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract AuctionFactory {
     address[] public auctions;
@@ -16,6 +17,11 @@ contract AuctionFactory {
 
     constructor() {
         emit FactoryCreated(address(this));
+    }
+
+    enum AuctionType {
+        BlindAuction,
+        VickreyAuction
     }
 
     function createBlindAuction(
@@ -38,26 +44,55 @@ contract AuctionFactory {
     }
 
     function createVickreyAuction(
-        address _nft,
+        ERC721 _nft,
         uint256 _nftId,
         address _beneficiary,
         EncryptedERC20 _tokenContract,
         address _contractOwner,
         uint biddingTime,
         bool isStoppable
-    ) public {
-        VickreyAuction newAuction = new VickreyAuction(
+    ) public returns (address newAuction) {
+        newAuction = getAuctionAddress(
             _nft,
             _nftId,
             _beneficiary,
             _tokenContract,
             _contractOwner,
             biddingTime,
-            isStoppable
+            isStoppable,
+            AuctionType.VickreyAuction
         );
-        auctions.push(address(newAuction));
-        vickreyAuctions.push(newAuction);
+
+        auctions.push(newAuction);
         emit AuctionCreated(address(newAuction), msg.sender, "VickreyAuction");
+        return newAuction;
+    }
+
+    // function that returns the auction address deployed by create2
+    function getAuctionAddress(
+        ERC721 _nft,
+        uint256 _nftId,
+        address _beneficiary,
+        EncryptedERC20 _tokenContract,
+        address _contractOwner,
+        uint biddingTime,
+        bool isStoppable,
+        AuctionType auctionType
+    ) public view returns (address) {
+        bytes memory bytecode = type(VickreyAuction).creationCode;
+        bytes32 salt = keccak256(
+            abi.encodePacked(
+                _nft,
+                _nftId,
+                _beneficiary,
+                _tokenContract,
+                _contractOwner,
+                biddingTime,
+                isStoppable,
+                auctionType
+            )
+        );
+        return address(uint160(uint256(keccak256(abi.encodePacked(bytecode, salt, address(this))))));
     }
 
     function getAuctions() public view returns (address[] memory) {
--- test/auctionFactory/AuctionFactory.ts
+++ test/auctionFactory/AuctionFactory.ts
@@ -1,13 +1,11 @@
 import { expect } from "chai";
 
-import type { AuctionFactory } from "../../types";
 import { deployEncryptedERC20Fixture } from "../encryptedERC20/EncryptedERC20.fixture";
+import { deployMockNFTFixture } from "../mockERC721/MockERC721.fixture";
 import { getSigners, initSigners } from "../signers";
 import { deployAuctionFactoryFixture } from "./AuctionFactory.fixture";
 
 describe("Auction Factory", function () {
-  let factory: AuctionFactory;
-
   before(async function () {
     await initSigners();
     this.signers = await getSigners();
@@ -19,34 +17,54 @@ describe("Auction Factory", function () {
     this.erc20 = contractErc20;
     this.contractERC20Address = await contractErc20.getAddress();
 
-    factory = await deployAuctionFactoryFixture(this.signers.alice);
+    const erc721 = await deployMockNFTFixture(this.signers.alice, this.signers.alice.address);
+    this.erc721 = erc721;
+
+    this.erc721.connect(this.signers.alice).safeMint(this.signers.alice.address);
+    this.contractERC721Address = await erc721.getAddress();
+
+    this.factory = await deployAuctionFactoryFixture(this.signers.alice);
+    this.factoryAddress = await this.factory.getAddress();
   });
 
   it("should deploy a blind auction", async function () {
-    const tx = await factory
+    const tx = await this.factory
       .connect(this.signers.alice)
       .createBlindAuction(this.signers.alice, this.contractERC20Address, this.signers.alice.address, 1000000, true);
     await tx.wait();
-    const auctions: `${string}`[] = await factory.getAuctions();
+    const auctions: `${string}`[] = await this.factory.getAuctions();
     expect(auctions.length).to.equal(1);
-    expect(auctions[0]).to.equal(await factory.blindAuctions(0));
+    expect(auctions[0]).to.equal(await this.factory.blindAuctions(0));
   });
 
   it("should deploy a vickrey auction", async function () {
-    const tx = await factory
+    const auctionAddress = await this.factory.getAuctionAddress(
+      this.contractERC721Address,
+      0,
+      this.signers.alice.address,
+      this.contractERC20Address,
+      this.signers.alice.address,
+      1000000,
+      true,
+      1,
+    );
+
+    const approveTx = await this.erc721.connect(this.signers.alice).setApprovalForAll(auctionAddress, true);
+    await approveTx.wait();
+    const tx = await this.factory
       .connect(this.signers.alice)
-      .createVickreyAuction(this.signers.alice, this.contractERC20Address, this.signers.alice.address, 1000000, true);
+      .createVickreyAuction(
+        this.contractERC721Address,
+        0,
+        this.signers.alice.address,
+        this.contractERC20Address,
+        this.signers.alice.address,
+        1000000,
+        true,
+      );
     await tx.wait();
-    const auctions: `${string}`[] = await factory.getAuctions();
+    const auctions: `${string}`[] = await this.factory.getAuctions();
     expect(auctions.length).to.equal(1);
-    expect(auctions[0]).to.equal(await factory.vickreyAuctions(0));
-  });
-  it("should return the auction address", async function () {
-    const tx = await factory
-      .connect(this.signers.alice)
-      .createVickreyAuction(this.signers.alice, this.contractERC20Address, this.signers.alice.address, 1000000, true);
-    await tx.wait();
-    const auctions: `${string}`[] = await factory.getAuctions();
-    expect(auctions.length).to.equal(1);
+    expect(auctions[0]).to.equal(auctionAddress);
   });
 });
